b1f5442c47809d0a2b6caad9982363a1b85e2a24
Khalil Taylor <khaliltaylor4@yahoo.com>
2025-10-16 11:48:34 -0400

    Update Double LEFT JOIN With UNION.sql
    added lab instructions to the bottom of the SQL file.

--- Double LEFT JOIN With UNION.sql
+++ Double LEFT JOIN With UNION.sql
@@ -22,3 +22,27 @@ LEFT JOIN BowlGame AS b
     ON c.CollegeID = b.WinningCollegeID
 
 ORDER BY Bowl IS NULL;
+
+/* INSTRUCTION
+
+The College table has the following columns:
+
+CollegeID - integer, primary key
+Name - variable-length string
+City - variable-length string
+State - two character string
+The BowlGame table has the following columns:
+
+BowlGameID - integer, primary key
+Bowl - variable-length string
+Stadium - variable-length string
+City - variable-length string
+State - two character string
+WinningCollegeID - integer, foreign key referencing CollegeID
+Write a SELECT statement listing bowl games, stadiums, and winning college names.  
+
+Include bowl games even if no matching wining college is recorded.
+Include college names even if the college does not have a bowl game win recorded.
+Hint: Your solution requires a UNION of two join queries.
+
+*/
